be9b02caa0962e3e81f903bac7f6902604be0867
Kyle Hutchinson <kyle1235@gmail.com>
2021-03-09 01:25:55 -0500

    Starting to get the button grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import { Menu, Loader, Button } from 'semantic-ui-react';
+import pokeballImage from './images/pokeball.svg';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -19,13 +21,37 @@ export default class App extends React.Component {
     }
 
     render() {
-        var image = this.state.image === null ? <></> : <img src={this.state.image} alt="Pokémon" />
+        var image = this.state.image === null ? <Loader active inline="centered" size="massive" /> : <img src={this.state.image} alt="Pokémon" height="100%" />
 
         return (
-            <div>
-                Hello World!
-                {image}
-            </div>
+            <>
+                <Menu inverted attached>
+                    <Menu.Item>
+                        <img src={pokeballImage} alt="Pokéball" />
+                    </Menu.Item>
+                    <Menu.Item>
+                        Pokémon Trivia Game
+                    </Menu.Item>
+                </Menu>
+
+                <div style={{ marginTop: "15px" }}>
+                    <div style={{ height: "20rem", display: "flex", justifyContent: "center" }}>
+                        {image}
+                    </div>
+                    <div style={{ textAlign: "center" }}>This should be in the centered</div>
+                    <div style={{ display: "grid", gridTemplateColumns: "auto auto auto" }}>
+                        <Button style={{ padding: "15px", margin: "15px" }}>A</Button>
+                        <Button>B</Button>
+                        <Button>C</Button>
+                        <Button>D</Button>
+                        <Button>E</Button>
+                        <Button>F</Button>
+                        <Button>G</Button>
+                        <Button>H</Button>
+                        <Button>I</Button>
+                    </div>
+                </div>
+            </>
         );
     }
 }
