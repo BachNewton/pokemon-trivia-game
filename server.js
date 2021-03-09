40c464eec7418ec680a373dec93dfb2b29667fe6
Kyle Hutchinson <kyle1235@gmail.com>
2021-03-09 00:21:43 -0500

    Good place to continue

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const socketIO = require('socket.io');
 
 const app = express();
 const server = http.Server(app);
-const io = socketIO(server);
+const io = socketIO(server, { cors: { origin: '*' } });
 
-app.set('port', 5000);
+const PORT = process.env.PORT || 5000;
+
+app.set('port', PORT);
 app.use('/static', express.static(__dirname + '/build/static'));
+
+const TriviaGame = require('./js/TriviaGame.js');
 
 // Routing
 app.get('/', function (_, response) {
@@ -17,15 +21,18 @@ app.get('/', function (_, response) {
 });
 
 // Start the server
-server.listen(5000, function () {
-    console.log('Starting server on port 5000');
+server.listen(PORT, function () {
+    console.log('Starting server on port:', PORT);
 });
+
+const players = {};
 
 // WebSocket handlers
 io.on('connection', function (socket) {
     console.log('new connection:', socket.id);
+    players[socket.id] = new TriviaGame();
 
     socket.on('disconnect', function () {
-        console.log('A player has disconnected! ID: ' + socket.id);
+        console.log('A player has disconnected! ID:', socket.id);
     });
 });
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 
 export default class App extends React.Component {
     componentDidMount() {
-        const socket = io();
+        const socket = process.env.NODE_ENV === 'production' ? io() : io('http://localhost:5000');
 
         socket.on('temp', () => {
             console.log('Hello!');
